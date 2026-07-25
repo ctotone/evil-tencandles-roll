68cfe095efaec83d8b2b51802a96b24380fcfd2c
Evilbram <ctotone@gmail.com>
2026-07-25 11:47:16 +0200

    update licence free use

--- scripts/fonts.js
+++ scripts/fonts.js
@@ -2,9 +2,9 @@
  * Enregistrement des polices du module auprès de Foundry.
  */
 
-export const THIRD_FONT_FAMILY = "3rd";
+export const THIRD_FONT_FAMILY = "special-elite";
 export const THIRD_FONT_URL =
-  "modules/evil-tencandles-roll/fonts/3rd%20Man.woff2";
+  "modules/evil-tencandles-roll/fonts/special-elite.woff2";
 
 export function registerModuleFonts() {
   CONFIG.fontDefinitions[THIRD_FONT_FAMILY] = {
